8a05737481cc9a248f65ab1fa8d69d00ba2029ea
Paul Horner <paul.horner.2002@gmail.com>
2022-12-27 12:09:31 +0100

    implemented SvgViewerApi

--- quteTestHorner/src/main/java/at/htl/boundary/SvgViewerApi.java
+++ quteTestHorner/src/main/java/at/htl/boundary/SvgViewerApi.java
@@ -25,7 +25,38 @@ public class SvgViewerApi {
     @Inject
     SvgViewerResource svgViewerResource;
 
+    @PUT
+    @Transactional
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response update(CircleDao circle) {
+        updateNode(circle.xPos(), circle.yPos(), circle.htmlId());
+        return Response.ok().build();
+    }
 
+    @POST
+    @Transactional
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Response postHtmlForm(
+            @FormParam("xPos") int xPos,
+            @FormParam("yPos") int yPos,
+            @FormParam("htmlIconName") String html
+    ) {
+        updateNode(xPos, yPos, html);
+        // Response.ok(svgViewerResource.renderSvg()).build();
+        return Response.seeOther(URI.create("/svg")).build();
+    }
 
-
+    private void updateNode(int xPos, int yPos, String html) {
+        var c = circleRepository.find("html_id", html).firstResult();
+        c.setxPos(xPos);
+        c.setyPos(yPos);
+        if (c.getLineConfig()==null){
+            var lines = lineConfigRepository.listAll();
+            for (LineConfig line:lines
+            ) {
+                line.setX2pos(xPos);
+                line.setY2pos(yPos);
+            }
+        }
+    }
 }
